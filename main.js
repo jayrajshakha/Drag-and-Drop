9c7163ab2b3652066e9180d42d6f274a991ef434
jay <jayrajshakha01@gmail.com>
2023-07-26 15:13:09 +0530

    Complite

--- main.js
+++ main.js
@@ -3,19 +3,28 @@ const items = document.querySelectorAll(".item");
 const rightBox = document.getElementById("right");
 const leftBox = document.getElementById("left");
 
-for(item of items) {
-
+for(item of items){
     item.addEventListener("dragstart",
-      function(e) {
-        let selected = e.target;
+    function(e){
+        let seleted = e.target;
+
         rightBox.addEventListener("dragover",
-         function(e) {
-            e.preventDefault()
-         }
-        )
-        rightBox.addEventListener("drop", function(e) {
-            rightBox.appendChild(selected);
-            selected = null;
-      }
-    )
+        function(e){
+            e.preventDefault();
+        })
+        rightBox.addEventListener("drop",
+        function(e){
+            rightBox.appendChild(seleted);
+            seleted = null;
+        })
+        leftBox.addEventListener("dragover",
+        function(e){
+            e.preventDefault();
+        })
+        leftBox.addEventListener("drop",
+        function(e){
+            leftBox.appendChild(seleted);
+            seleted = null;
+        })
+    })
 }
